d1e5b48ab4243e275ce6fe70d9c0b8ed15e21825
H-rafael <35052244+H-rafael@users.noreply.github.com>
2019-02-15 14:28:24 +0800

    Update comments.php

--- comments.php
+++ comments.php
@@ -34,7 +34,7 @@ function threadedComments($comments, $options) {
             </div>
             <div class="comment-body">
                 <div class="meta-data">
-                    <span class="pull-right">
+                    <span class="pull-right"     onclick="return addComment.moveForm( "comment-24", "24", "respond", "222" )">
                         <?php $comments->reply('<i class="fa fa-mail-reply-all"></i>回复'); ?>
                     </span>
 
@@ -73,9 +73,6 @@ function threadedComments($comments, $options) {
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php commentReply($this); ?>
 
-
-
-
 <div id="comments" class="comments-area thw-sept wow bounceInUp animated" style="visibility: visible; animation-name: bounceInUp;">
     <h3 class="comments-heading"><?php $this->commentsNum(_t('0 Comments'), _t('1 Comments'), _t('%d Comments')); ?> </h3>
     <?php if($this->allow('comment')): ?>
@@ -88,53 +85,40 @@ function threadedComments($comments, $options) {
     <div class="pagenav text-center"></div>
 </div><!-- Post comment end -->
 
-<h3 class="title-normal thw-sept text-center wow swing animated" id="<?php $this->respondId(); ?>" style="visibility: visible; animation-name: swing;">留下你的评论</h3>
-<div class="clearfix text-center">
+<h3 class="title-normal thw-sept text-center wow swing animated"   style="visibility: visible; animation-name: swing;">留下你的评论</h3>
+<div class="clearfix text-center" >
 <!--    *评论支持代码高亮&lt;pre class="prettyprint linenums"&gt;代码&lt;/pre&gt;-->
 </div>
-
-<div id="respond">
-    <form id="commentform" name="commentform" action="<?php $this->commentUrl() ?>" role="form" method="post">
-        <div class="row justify-content-center mb10"><div class="col-md-4"><a id="cancel-comment-reply-link" href="javascript:;" class="btn btn-sm btn-info" style="display:none;">取消回复</a></div></div>
-
-        <div class="row" id="comment-author-info">
+<div  class="respond" id="<?php $this->respondId(); ?>" >
+    <form method="post" action="<?php $this->commentUrl() ?>" id="commentform" role="form">
+        <div class="row">
             <div class="col-md-4">
                 <div class="form-group">
-                    <input placeholder="（必填）昵称" type="text" name="author" id="author" class="form-control" value="<?php $this->remember('author'); ?>" required />
-
-<!--                    <input type="text" name="author" id="author" class="form-control" value="" placeholder="昵称 *" tabindex="1">-->
-                </div>
-            </div><!-- Col 4 end -->
-            <div class="col-md-4">
-                <div class="form-group">
-                    <input placeholder="<?php echo $this->options->commentsRequireMail ? '（必填）' : '（选填）';echo '邮箱'; ?>" type="email" name="mail" id="mail" class="form-control" value="<?php $this->remember('mail'); ?>"
-                        <?php if ($this->options->commentsRequireMail): ?> required
-                        <?php endif; ?>/>
-<!--                    <input type="email" name="email" id="email" class="form-control" value="" placeholder="邮箱 *" tabindex="2">-->
+                    <input placeholder="（必填）昵称" type="text" name="author" id="author" tabindex="1" class="form-control" value="<?php $this->remember('author'); ?>" required />
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="form-group">
-                    <input type="url" name="url" id="url" class="form-control" placeholder="<?php echo $this->options->commentsRequireURL ? '（必填）' : '（选填）';echo '网站'; ?>"
-                           value="<?php $this->remember('url'); ?>" <?php
-                    if ($this->options->commentsRequireURL): ?> required
-                    <?php endif; ?>/>
+                    <input placeholder="<?php echo $this->options->commentsRequireMail ? '（必填）' : '（选填）';echo '邮箱'; ?>"  tabindex="2" type="email" name="mail" id="mail" class="form-control" value="<?php $this->remember('mail'); ?>"<?php if ($this->options->commentsRequireMail): ?> required<?php endif; ?>/>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <input type="url" name="url" id="url" class="form-control" placeholder="<?php echo $this->options->commentsRequireURL ? '（必填）' : '（选填）';echo '网站'; ?>" value="<?php $this->remember('url'); ?>" <?php if ($this->options->commentsRequireURL): ?> required<?php endif; ?>/>
                 </div>
             </div>
         </div>
+
         <div class="row">
             <div class="col-md-12">
                 <div class="form-group">
-                    <textarea class="form-control  required-field" rows="5" name="text" id="comment" tabindex="4" placeholder="你可以在这里输入评论内容..."><?php $this->remember('text'); ?></textarea><div id="loading" style="display: none;"><i class="fa fa-spinner fa-spin"></i> 正在提交, 请稍候...</div><div id="error" style="display: none;">#</div>
+                    <textarea class="form-control  required-field" rows="5" name="text" id="text" tabindex="4" placeholder="你可以在这里输入评论内容..."><?php $this->remember('text'); ?></textarea><div id="loading" style="display: none;"><i class="fa fa-spinner fa-spin"></i> 正在提交, 请稍候...</div><div id="error" style="display: none;">#</div>
                 </div>
-            </div><!-- Col 12 end -->
-        </div><!-- Form row end -->
+            </div>
+        </div>
         <div class="clearfix text-center">
-<!--            <div class="comt-tips"><input type="hidden" name="comment_post_ID" value="283" id="comment_post_ID">-->
-<!--                <input type="hidden" name="comment_parent" id="comment_parent" value="0">-->
-<!--            </div>-->
-            <button class="btn btn-1 btn-outline-success" name="submit" type="submit" id="submit" tabindex="5">发表评论</button>
+            <button class="btn btn-1 btn-outline-success"  type="submit" id="submit" tabindex="5">发表评论</button>
         </div>
     </form>
-
 </div>
+   
